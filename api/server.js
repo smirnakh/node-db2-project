ee13853ba12871fa30cdb79c4ac222f6d8264d17
smirnakh <99379073+smirnakh@users.noreply.github.com>
2022-09-19 11:19:24 -0700

    done

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
-const express = require("express")
+const express = require('express');
 
-const server = express()
+const carsRouter = require('./cars/cars-router');
 
-// DO YOUR MAGIC
+const server = express();
 
-module.exports = server
+server.use(express.json());
+
+server.use('/api/cars', carsRouter);
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'Not Found' });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+module.exports = server;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+  {
+    vin: '11111111111111111',
+    make: 'toyota',
+    model: 'prius',
+    mileage: 21500,
+    title: 'clean',
+    transmission: 'manual',
+  },
+  {
+    vin: '22222222222222222',
+    make: 'toyota',
+    model: 'corola',
+    mileage: 36000,
+    title: 'salvage',
+  },
+  {
+    vin: '33333333333333333',
+    make: 'ford',
+    model: 'focus',
+    mileage: 41509,
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate();
+  await knex('cars').insert(cars);
+};
